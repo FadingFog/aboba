32643bf32626dc7948fb58fa1c5fde77ee0f2a9e
FadingFog <alexey.guzlov@innowise.com>
2024-06-10 15:39:19 +0200

    Fixed typing in new_session function

--- core/dependencies.py
+++ core/dependencies.py
@@ -3,7 +3,7 @@ from functools import partial
 from typing import AsyncGenerator
 
 from fastapi import FastAPI
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine, async_scoped_session
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from app.models import BaseA, BaseB
 
@@ -42,13 +42,13 @@ def create_session_maker() -> async_sessionmaker[AsyncSession]:
 
 
 async def new_session(
-    scoped_session_maker: async_scoped_session[AsyncSession]
+    session_maker: async_sessionmaker[AsyncSession]
 ) -> AsyncGenerator[AsyncSession, None]:
-    async with scoped_session_maker() as session:
+    async with session_maker() as session:
         yield session
         await session.commit()
 
 
 def init_dependencies(app: FastAPI):
-    scoped_session_maker = create_session_maker()
-    app.dependency_overrides[AsyncSession] = partial(new_session, scoped_session_maker)
+    session_maker = create_session_maker()
+    app.dependency_overrides[AsyncSession] = partial(new_session, session_maker)
