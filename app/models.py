10dc9b1472c5a459242c34b7da0d8e09d0bce448
FadingFog <alexey.guzlov@innowise.com>
2024-06-10 17:14:04 +0200

    Refactored models

--- app/models.py
+++ app/models.py
@@ -2,25 +2,36 @@ from sqlalchemy import String
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
-class BaseA(DeclarativeBase):
-    __abstract__ = True
-
+class Base:
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
 
 
-class BaseB(DeclarativeBase):
+# Declare DeclarativeBases & Models for each engine (data source)
+# according to https://docs.sqlalchemy.org/en/20/orm/persistence_techniques.html#session-partitioning
+
+class BaseA(Base, DeclarativeBase):
     __abstract__ = True
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+
+class BaseB(Base, DeclarativeBase):
+    __abstract__ = True
 
 
-class SomeDataA(BaseA):
-    __tablename__ = 'some_data'
+class BaseC(Base, DeclarativeBase):
+    __abstract__ = True
 
+
+class SomeDataCommon:
     name: Mapped[str] = mapped_column(String(255))
 
 
-class SomeDataB(BaseB):
+class SomeDataA(SomeDataCommon, BaseA):
     __tablename__ = 'some_data'
 
-    name: Mapped[str] = mapped_column(String(255))
+
+class SomeDataB(SomeDataCommon, BaseB):
+    __tablename__ = 'some_data2'
+
+
+class SomeDataC(SomeDataCommon, BaseC):
+    __tablename__ = 'some_data3'
