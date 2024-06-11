f9c1f506d1abe0f6d9da8173d2cc25f4bd2f45e3
FadingFog <alexey.guzlov@innowise.com>
2024-06-11 18:14:54 +0200

    Moved base fixtures to conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,20 @@
 import asyncio
+import os
+from functools import partial
+from typing import AsyncGenerator, Type
 
 import pytest
+from dotenv import load_dotenv
+from fastapi.testclient import TestClient
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, AsyncEngine
+
+from app.main import create_app
+from app.models import BaseA, BaseB, BaseC
+from core.dependencies import create_session_maker, get_binds, BaseModel
 
 pytest_plugins = ["tests.fixtures"]
+
+load_dotenv()
 
 
 @pytest.fixture(scope='session', autouse=True)
@@ -11,6 +23,38 @@ def event_loop(request):
     loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
     loop.close()
+
+
+@pytest.fixture(scope='session')
+async def test_binds() -> dict[Type[BaseModel], AsyncEngine]:
+    database_uri = os.getenv("TEST_DATABASE_URI")
+    sources_mapping = {  # Map sources URI per model
+        BaseA: database_uri,
+        BaseB: database_uri,
+        BaseC: database_uri,
+    }
+    return get_binds(sources_mapping)
+
+
+@pytest.fixture
+async def test_session_maker(test_binds) -> async_sessionmaker[AsyncSession]:
+    return create_session_maker(binds=test_binds)
+
+
+@pytest.fixture
+async def test_session(test_session_maker) -> AsyncGenerator[AsyncSession, None]:
+    async with test_session_maker() as session:
+        yield session
+
+
+@pytest.fixture(scope='session')
+def client(test_binds) -> TestClient:
+    app = create_app()
+    app.dependency_overrides[AsyncSession] = partial(test_session, test_session_maker)
+    app.dependency_overrides[async_sessionmaker] = partial(create_session_maker, test_binds)
+
+    test_client = TestClient(app)
+    return test_client
 
 
 @pytest.fixture(scope='function', autouse=True)
